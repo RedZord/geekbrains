6edade78e53e58138d99e3de0ecb46174023a52a
Stas <ly150652@gmail.com>
2020-12-07 15:17:44 +1000

    Update HW_1_1.py file
    Completing Task 1

---
+++ HW_1/HW_1_1.py
@@ -0,0 +1,11 @@
+
+a = 43
+b = 'Dude'
+c = False
+
+print(f'{a}, {b}, {c}.')
+
+number = int(input('Введите любое число: '))
+string = input('Введите любое число: ')
+
+print(f'{number}, {string}.')
